f56817a380624d63460e583b4662b90faf89a9c6
Joseph Modi <modithejoe@gmail.com>
2023-08-14 23:14:36 +0300

    My commit

--- passwords.py
+++ passwords.py
@@ -2,9 +2,9 @@ import pickle
 from pathlib import Path
 import streamlit_authenticator as stauth
 
-names = ["Joseph Modi", "Emmah Wavinya", "Raissa A", "Abdul M", "Fuji Cheruiyot", "Eva Kimani", "Yusuf Kariuki", "George Orembo"]
-usernames = ["adinoself", "missdivine", "raissanbg", "abdulnbg", "fujicheruiyot", "evakimani", "yusufkariuki", "georgeorembo"]
-passwords = ["123", "456", "transtest1#", "transtest2#", "transtest3#", "transtest4#", "transtest5#", "transtest6#"]
+names = ["Joseph Modi", "Emmah Wavinya", "Fuji Cheruiyot", "Eva Kimani", "Yusuf Kariuki", "George Orembo"]
+usernames = ["adinoself", "missdivine", "fujicheruiyot", "evakimani", "yusufkariuki", "georgeorembo"]
+passwords = []
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
