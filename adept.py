3f925b71da79b4d89846fa2dd9028579922197b8
adinoself <94802252+adinoself@users.noreply.github.com>
2023-08-14 23:15:48 +0300

    Update adept.py

--- adept.py
+++ adept.py
@@ -26,7 +26,7 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # --- USER AUTHENTICATION ---
-names = ["Joseph Modi", "Transcriber 1", "Transcriber 2", "Transcriber 3", "Transcriber 4", "Transcriber 5", "Transcriber 6", "Transcriber 7", "Transcriber 8", "Transcriber 9", "Transcriber 10"]
+names = ["Joseph Modi", "Emmah Wavinya", "Fuji Cheruiyot", "Eva Kimani", "Yusuf Kariuki", "George Orembo"]
 usernames = ["adinoself", "transcriber1", "transcriber2", "transcriber3", "transcriber4", "transcriber5", "transcriber6", "transcriber7", "transcriber8", "transcriber9", "transcriber10"]
 credentials = {"usernames": {}}
 
